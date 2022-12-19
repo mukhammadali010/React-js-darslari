676ceb0c43ddffa172ea1900aa607161d4839266
Пользователь <muhammadalinosirov34@gmail.com>
2022-12-19 07:54:54 +0500

    vazifa_4 complated

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,52 +1,80 @@
 import React from "react";
 import "./style.css";
-
 class Task extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-        { id: 1, title: "Something here " },
-        { id: 2, title: "React" },
-        { id: 3, title: "Angular" },
-        { id: 4, title: "Vue" },
-      ],
+      subjects: [],
+      value: "",
     };
   }
   render() {
-    // const addList =()=>{
-    //     for(let i = 0; i < this.state.data.length; i++){
-    //         console.log(this.state.data[i]);
-    //         // this.state.data.push({title: "hello"}) 
-
-    //     }
-    // }
-    // const inpValue = (e)=>{
-    //     console.log(e.target.value);
-    //     this.setState({title: e.target.value})
-    // }
+    const deleting = (id) => {
+      console.log(id);
+      const res = this.state.subjects.filter((value) => value.id !== id);
+      this.setState({ subjects: res });
+    };
+    const onSearch = (e) => {
+      const res = this.state.subjects.filter((value) =>
+        value.title.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      this.setState({ subjects: res });
+    };
+    const onAdd = () => {
+      const obj = {
+        title: this.state.value,
+        id:  Math.trunc(Math.random() * 1000 + 1),
+      };
+      console.log(obj.id);
+      const res = this.state.subjects;
+      res.unshift(obj);
+      this.setState({ subjects: res });
+      this.setState({ value: "" });
+    };
+    const getValue = (e) => {
+      this.setState({ value: e.target.value });
+    };
     return (
       <div className="container">
         <div className="hero">
-          <h1>{this.state.data.length} Tasks</h1>
-          <p>{this.state.data.length} Remain</p>
+          <h1>{this.state.subjects.length} Tasks</h1>
+          <p>{this.state.subjects.length} Remain</p>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={onSearch}
+            name=""
+            id="searching"
+          />
         </div>
         <div className="main">
-          {this.state.data.map((value) => {
+          {this.state.subjects.map((value) => {
             return (
-              <div className="wrapper">
+              <div className="wrapper" key={value.id}>
                 <div className="flex">
                   <input type="checkbox" id="" className="inp" />
                   <p className="title">{value.title}</p>
                 </div>
-                <button className="btn">Delete</button>
+                <button className="btn" onClick={() => deleting(value.id)}>
+                  Delete
+                </button>
               </div>
             );
           })}
         </div>
         <div className="footer">
-          <input type="text" name="" id=""  placeholder="Add Todo..." className="inpAdd"/>
-          <button className="btn" >Add</button>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Add Todo..."
+            className="inpAdd"
+            value={this.state.value}
+            onChange={getValue}
+          />
+          <button className="btn" onClick={onAdd}>
+            Add
+          </button>
         </div>
       </div>
     );
